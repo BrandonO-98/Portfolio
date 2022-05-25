84438d447ef14e88e5d142e425a11943885fceb2
BrandonO-98 <brandonshew98@gmail.com>
2022-05-24 21:25:57 -0400

    A professional img is required

--- src/components/sections/hero/Hero.jsx
+++ src/components/sections/hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import closeup from 'assets/images/closeup.jpg';
-import bodyshot from 'assets/images/bodyshot2.jpg';
+import bodyshot from 'assets/images/formal.jpg';
 
 export default function Hero() {
   return (
@@ -13,12 +13,12 @@ export default function Hero() {
             specializing in the front-end.
           </p>
           <ul className="grid grid-cols-2 items-center justify-items-center">
-            <li className="grid"><a href="/" className="bg-blue-400 text-white px-8 py-2 mr-1 rounded-full">Resume</a></li>
+            <li className="grid"><a href="/" className="bg-blue-500 text-white px-8 py-2 mr-1 rounded-full">Resume</a></li>
             <li className="grid"><a href="/" className="border-black border px-8 py-2 ml-1 rounded-full">Projects</a></li>
           </ul>
         </div>
         <div className="grid bg-blue-200 h-full w-full items-center justify-items-center">
-          <img src={bodyshot} alt="Close-up of Brandon" className="h-10/12 object rounded-full border-4 border-white" />
+          <img src={bodyshot} alt="Close-up of Brandon" className="w-5/6 object-fit rounded-full border-4 border-white mx-4" />
         </div>
 
       </div>
