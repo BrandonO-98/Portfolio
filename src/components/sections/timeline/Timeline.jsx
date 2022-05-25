13b7fa7261ae968476bc4938f8cdc867a06a46b7
BrandonO-98 <brandonshew98@gmail.com>
2022-05-25 17:58:35 -0400

    Added pdf download. Fiz x-scroll.

--- src/components/sections/timeline/Timeline.jsx
+++ src/components/sections/timeline/Timeline.jsx
@@ -7,29 +7,38 @@ import timelineData from './TimelineData';
 
 export default function Timeline() {
   return (
-    <VerticalTimeline lineColor="black">
-      {timelineData.map((timelineEvent) => (
-        <VerticalTimelineElement
-          key={timelineEvent.id}
-          className="vertical-timeline-element--work"
-          contentStyle={timelineEvent.icon === 'work' ? { background: '#DCFCE7', color: '#334155', border: '4px solid #22C55E' }
-            : { background: '#FECDD3', color: '#334155', border: '4px solid #F43F5E' }}
-          contentArrowStyle={timelineEvent.icon === 'work' ? { borderRight: '10px solid  #22C55E' }
-            : { borderRight: '10px solid  #F43F5E' }}
-          date={timelineEvent.date}
-          iconStyle={timelineEvent.icon === 'work' ? { background: 'white', border: '4px solid #22C55E' }
-            : { background: 'white', border: '4px solid #F43F5E' }}
-          icon={timelineEvent.icon === 'work' ? <WorkIcon /> : <SchoolIcon />}
-        >
-          <h2 className="vertical-timeline-element-title">{timelineEvent.title}</h2>
-          <h3 className="vertical-timeline-element-subtitle">{timelineEvent.location}</h3>
-          <ul className="list-disc ml-4">
-            {[...timelineEvent.desc].map((bullet) => <li>{bullet}</li>)}
-          </ul>
-          <a href="/" className="">Button</a>
-        </VerticalTimelineElement>
-      ))}
+    <div className="grid w-full items-center justify-content-center">
+      <a
+        href="./Resume_Developer.pdf"
+        download="Resume_Developer.pdf"
+        className="justify-self-end mr-96 ease-in duration-300 border border-green-500 bg-green-500 text-white px-8 py-2 rounded-full font-bold
+                  hover:text-black hover:bg-white"
+      >
+        Download Resume PDF
 
-    </VerticalTimeline>
+      </a>
+      <VerticalTimeline lineColor="black">
+        {timelineData.map((timelineEvent) => (
+          <VerticalTimelineElement
+            key={timelineEvent.id}
+            className="vertical-timeline-element--work"
+            contentStyle={timelineEvent.icon === 'work' ? { background: '#DCFCE7', color: '#334155', border: '4px solid #22C55E' }
+              : { background: '#FECDD3', color: '#334155', border: '4px solid #F43F5E' }}
+            contentArrowStyle={timelineEvent.icon === 'work' ? { borderRight: '10px solid  #22C55E' }
+              : { borderRight: '10px solid  #F43F5E' }}
+            date={timelineEvent.date}
+            iconStyle={timelineEvent.icon === 'work' ? { background: 'white', border: '4px solid #22C55E' }
+              : { background: 'white', border: '4px solid #F43F5E' }}
+            icon={timelineEvent.icon === 'work' ? <WorkIcon /> : <SchoolIcon />}
+          >
+            <h2 className="vertical-timeline-element-title">{timelineEvent.title}</h2>
+            <h3 className="vertical-timeline-element-subtitle">{timelineEvent.location}</h3>
+            <ul className="list-disc ml-4">
+              {[...timelineEvent.desc].map((bullet) => <li>{bullet}</li>)}
+            </ul>
+          </VerticalTimelineElement>
+        ))}
+      </VerticalTimeline>
+    </div>
   );
 }
